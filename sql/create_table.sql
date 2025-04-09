d237a4b637bc1896a40c347345ff4159a72be1ff
yzy <2374614939@qq.com>
2025-04-09 23:04:31 +0800

    second

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -56,22 +56,6 @@ create table user_team
 ) comment '用户队伍关系';
 
 
--- 标签表（可以不创建，因为标签字段已经放到了用户表中）
-create table tag
-(
-    id         bigint auto_increment comment 'id'
-        primary key,
-    tagName    varchar(256) null comment '标签名称',
-    userId     bigint null comment '用户 id',
-    parentId   bigint null comment '父标签 id',
-    isParent   tinyint null comment '0 - 不是, 1 - 父标签',
-    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
-    isDelete   tinyint  default 0 not null comment '是否删除',
-    constraint uniIdx_tagName
-        unique (tagName)
-) comment '标签';
-
 
 create index idx_userId
     on tag (userId);
